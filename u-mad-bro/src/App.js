415a38454e17c1eee9e36a426aa9b629250cad9d
DentonWarnock <dentonwarnock@gmail.com>
2019-10-16 22:20:54 -0700

    Handling merge conflict with App.js

--- u-mad-bro/src/App.js
+++ u-mad-bro/src/App.js
@@ -10,18 +10,10 @@ import PrivateRoute from "./components/PrivateRoute.js";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-=======
     <main>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/signup" component={SignUp} />
     </main>
->>>>>>> 454aabd8deb8c0f0569414395be488f537b4a8a4
   );
 }
 
