23dcb951328b2f2dfcf82458b8eff4928d3137dd
DentonWarnock <dentonwarnock@gmail.com>
2019-10-21 19:56:19 -0700

    Fixed missing logo import, removed search bar route, added search bar to dashboard.

--- src/Layout/Dashboard.js
+++ src/Layout/Dashboard.js
@@ -1,11 +1,13 @@
 import React, { useContext } from "react";
 import Feed from "../components/Feed.js";
 import NavBar from "./NavBar.js";
+import SearchBar from "../components/SearchBar.js";
 
 const Dashboard = props => {
   return (
     <>
       <NavBar />
+      <SearchBar />
       <Feed />
     </>
   );
